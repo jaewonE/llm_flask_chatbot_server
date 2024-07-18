e2c94e1f082f0fc7d209fe87a318907c4b29fd77
jaewonE <importjaewonE@gmail.com>
2024-07-18 14:26:24 +0900

    Add access code to signup

--- apis/user.py
+++ apis/user.py
@@ -8,11 +8,13 @@ user_bp = Blueprint('user', __name__)
 
 
 @user_bp.route('/user/signup', methods=['POST'])
-@validate_json(['user_name', 'user_password'])
+@validate_json(['user_name', 'user_password', 'access_code'])
 def signup_api():
-    user_name, user_password = g.data['user_name'], g.data['user_password']
+    user_name, user_password, access_code = g.data['user_name'], g.data['user_password'], g.data['access_code']
     if get_user_info(user_name):
         return jsonify({'status': 'error', 'message': 'User already exists.'}), 400
+    if access_code != 'billy':
+        return jsonify({'status': 'error', 'message': 'Access code incorrect.'}), 400
     hashed_password = bcrypt.hashpw(
         user_password.encode(), bcrypt.gensalt()).decode()
     append_user_info(user_name, hashed_password)
